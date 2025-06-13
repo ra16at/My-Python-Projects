7dd928131db3d1551f75c4ab27ef7fdb343362eb
Rahat <ismailhossenrahat01710635470@gmail.com>
2025-06-14 00:08:34 +0600

    Fix Factorial.py not working correctly

--- Factoral.py
+++ Factoral.py
@@ -1,11 +1,19 @@
-num = int(input("=> "))
-factor_of = int(input("[Factor of] => "))
+# Prompt user for a number
+num = int(input("Enter a number: "))
 
-def factoral(num, factor_of):
+# Prompt user for a potential factor
+factor_of = int(input("Enter a potential factor: "))
+
+def check_factor(num, factor_of):
+    """
+    Checks if 'factor_of' is a factor of 'num'.
+    If not, prompts the user to enter a new number until it is.
+    """
     while num % factor_of != 0:
         print("Try again")
-        num = int(input("=> "))
+        num = int(input("Enter a number: "))
     else:
         print("%d is a factor of %d" % (factor_of, num))
 
-factoral(num, factor_of)
+# Call the function
+check_factor(num, factor_of)
